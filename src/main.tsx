e94c23685be9a8d4431ccc02829261f68aa00e32
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-02-12 05:31:09 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./contexts/AuthContext.tsx";
+
+import "./i18n.ts";
+import ThemeContextProvider from "./contexts/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <App />
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
