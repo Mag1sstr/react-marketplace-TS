7e337405f45ebd8663cd1180bbff4ec5a822fdbc
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-02-13 06:16:12 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import AppRoutes from "./components/AppRoutes/AppRoutes";
-import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
-import Spinner from "./components/Spinner/Spinner";
 
 function App() {
-  // console.log(data);
-
   return (
     <>
       <Header />
