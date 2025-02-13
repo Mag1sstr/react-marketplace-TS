041ad76eb12ffdb25227c1184b5a4601e62eafad
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-02-13 06:16:18 +0500

    Update AppRoutes.tsx

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import ProductPage from "../ProductPage/ProductPage";
 import MainPage from "../MainPage/MainPage";
 // import Categories from "../Categories/Categories";
 import CategoryPage from "../CategoryPage/CategoryPage";
+import Basket from "../Basket/Basket";
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
       <Route path="/" element={<MainPage />} />
       <Route path="/product/:id" element={<ProductPage />} />
       <Route path="/category/:id" element={<CategoryPage />} />
+      <Route path="/basket" element={<Basket />} />
     </Routes>
   );
 }
