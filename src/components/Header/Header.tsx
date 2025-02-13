29138a89c084e017fa150ee2ffca9b84d6235fd1
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-02-13 06:17:19 +0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
 import styles from "./style.module.css";
 import Categories from "../Categories/Categories";
 import logo from "../../images/logo.png";
-import cart from "../../images/cart.png";
+import cartImage from "../../images/cart.png";
 import loginIcon from "../../images/login.png";
 import LoginModal from "../LoginModal/LoginModal";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { IContext, useTheme } from "../../contexts/ThemeContext";
 
 export default function Header() {
   const [clickLogin, setClickLogin] = useState(false);
+  const [lang, setLang] = useState("EN");
   const navigate = useNavigate();
 
   const { user, setToken, setUser } = useContext(AuthContext);
+  const { dark, setDark, cart }: IContext = useTheme();
+
+  const { i18n } = useTranslation();
 
   return (
-    <header>
+    <header className={dark ? "dark" : ""}>
       <LoginModal clickLogin={clickLogin} setClickLogin={setClickLogin} />
       <div className={styles.header}>
         <div className="conteiner">
@@ -50,7 +56,27 @@ export default function Header() {
                   alt=""
                 />
               )}
-              <img className={styles.cart} src={cart} alt="" />
+              <div className={styles.cart}>
+                <img
+                  onClick={() => navigate("/basket")}
+                  className={styles.cartImage}
+                  src={cartImage}
+                  alt=""
+                />
+                {cart.length > 0 && (
+                  <div className={styles.count}>{cart.length}</div>
+                )}
+              </div>
+              <button
+                className={styles.lang}
+                onClick={() => {
+                  setLang(lang === "EN" ? "RU" : "EN");
+                  i18n.changeLanguage(lang === "EN" ? "ru" : "en");
+                }}
+              >
+                {lang}
+              </button>
+              <button onClick={() => setDark(!dark)}>Theme</button>
             </div>
           </div>
         </div>
